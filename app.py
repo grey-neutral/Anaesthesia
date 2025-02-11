8b1353a8727e3c70a49ddb1a121ea01b2f734785
grey-neutral <brinz.janet@gmail.com>
2025-02-11 21:20:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,8 @@ def main():
     st.write(
         "This Python application helps to identify the right type of Articaine-based anaesthesia (Ultracain) "
         "by evaluating contraindications based on patient medical conditions and calculates the maximum allowable "
-        "dosage based on the patient's body weight."
+        "dosage based on the patient's body weight. "
+        " "
         "This app is for informational purposes only, not intended for medical use!"
     )
 
